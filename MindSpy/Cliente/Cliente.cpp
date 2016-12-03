f3f520efedf03e531046147a990d7963d9e7a0bd
Carlos D. Alvarez <CarlosD_17@yahoo.com>
2016-12-02 21:24:49 -0400

    Actualización proyectos

---
+++ MindSpy/Cliente/Cliente.cpp
@@ -0,0 +1,8 @@
+#include "Cliente.h"
+int APIENTRY wWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPWSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	return 0;
+}
--- SpyMindSol/SpyMindSol/SpyMindSol.cpp
+++
@@ -1,9 +0,0 @@
-#include <Windows.h>
-
-int APIENTRY wWinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPWSTR    lpCmdLine,
-	int       nCmdShow)
-{
-}
