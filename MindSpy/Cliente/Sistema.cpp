324391e85403c26d4ad98665167c56eda6f38796
Carlos D. Alvarez <CarlosD_17@yahoo.com>
2016-12-04 20:30:37 -0400

    Update

--- MindSpy/Cliente/Sistema.cpp
+++ MindSpy/Cliente/Sistema.cpp
@@ -2,50 +2,37 @@
 
 namespace MindSpy
 {
-	class Sistema {
-	private:
-		struct stSystemInfo {
-			USHORT VersionMayor;
-			USHORT VersionMenor;
-			UINT32 Build;
-			string NombreOS;
-			string TipoLicencia;
-			string MAC;
-			string NombreUsuario;
-			string NombreInstalador;
-			bool EsWindowsServer;
-		} SysInfo;
+	char* Sistema::ObtenerMAC()
+	{
+		PIP_ADAPTER_INFO AdapterInfo;
+		DWORD dwBufLen = sizeof(AdapterInfo);
+		static char mac_addr[18];
+		AdapterInfo = (IP_ADAPTER_INFO *)malloc(dwBufLen);
 
-		string Sistema::ObtenerMAC()
-		{
-			PIP_ADAPTER_INFO AdapterInfo;
-			string r = "";
-			DWORD dwBufLen = sizeof(AdapterInfo);
-			static char mac_addr[18];
-
-			AdapterInfo = (IP_ADAPTER_INFO *)malloc(dwBufLen);
-
-			if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == ERROR_BUFFER_OVERFLOW) {
-				free(AdapterInfo);
-				AdapterInfo = (IP_ADAPTER_INFO*)malloc(dwBufLen);
-			}
-
-			if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == NO_ERROR) {
-				do {
-					if (strcmp(AdapterInfo->IpAddressList.IpAddress.String, "0.0.0.0"))
-					{
-						sprintf(mac_addr, "%02X:%02X:%02X:%02X:%02X:%02X",
-							AdapterInfo->Address[0], AdapterInfo->Address[1],
-							AdapterInfo->Address[2], AdapterInfo->Address[3],
-							AdapterInfo->Address[4], AdapterInfo->Address[5]);
-
-						return mac_addr;
-					}
-					AdapterInfo = AdapterInfo->Next;
-				} while (AdapterInfo);
-			}
+		if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == ERROR_BUFFER_OVERFLOW) {
 			free(AdapterInfo);
+			AdapterInfo = (IP_ADAPTER_INFO*)malloc(dwBufLen);
 		}
 
+		if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == NO_ERROR) {
+			do {
+				if (strcmp(AdapterInfo->IpAddressList.IpAddress.String, "0.0.0.0"))
+				{
+					sprintf(mac_addr, "%02X:%02X:%02X:%02X:%02X:%02X",
+						AdapterInfo->Address[0], AdapterInfo->Address[1],
+						AdapterInfo->Address[2], AdapterInfo->Address[3],
+						AdapterInfo->Address[4], AdapterInfo->Address[5]);
+
+					return mac_addr;
+				}
+				AdapterInfo = AdapterInfo->Next;
+			} while (AdapterInfo);
+		}
+		free(AdapterInfo);
+	}
+
+	UINT32 Sistema::ObtenerVersionWindows()
+	{
+		
 	}
 }
--- MindSpy/Cliente/Sistema.h
+++ MindSpy/Cliente/Sistema.h
@@ -7,9 +7,22 @@ using namespace std;
 
 namespace MindSpy
 {
+	struct stSystemInfo {
+		USHORT VersionMayor;
+		USHORT VersionMenor;
+		UINT32 Build;
+		char NombreOS[64];
+		char TipoLicencia[64];
+		char MAC[18];
+		char NombreUsuario[64];
+		char NombreInstalador[64];
+		bool EsWindowsServer;
+	};
+
 	class Sistema
 	{
 	public:
-		string ObtenerMAC();
+		char* ObtenerMAC();
+		UINT32 ObtenerVersionWindows();
 	};
 }
