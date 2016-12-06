a29a4da7cb82918db64331a6bb48ef3caf6653b3
Carlos D. Alvarez <CarlosD_17@yahoo.com>
2016-12-05 23:38:34 -0400

    Update

--- MindSpy/Cliente/Cliente.cpp
+++ MindSpy/Cliente/Cliente.cpp
@@ -1,11 +1,11 @@
 #include "Cliente.h"
 
-DWORD WINAPI HiloProc(LPARAM l);
-
 //int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR    lpCmdLine, int nCmdShow)
 int main()
 {
-	Conector cn("127.0.0.1", 9900);
+	string ip;
+	cout << "Ingrese la IP: "; cin >> ip;
+	Conector cn(ip, 9900);
 	while (cn.Listo())
 	{
 		Sleep(1000);
