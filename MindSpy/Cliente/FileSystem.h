4391b691624627bb56ec5ee4d8a05db5670a7343
AnnyBone <boneanny278@gmail.com>
2016-12-06 15:49:14 -0400

    Update

--- MindSpy/Cliente/FileSystem.h
+++ MindSpy/Cliente/FileSystem.h
@@ -5,14 +5,53 @@
 
 namespace MindSpy
 {
+
+#define  DIR_ROOT_SYSTEM				0x01 // C:\WINDOWS
+#define  DIR_ROOT_DESKTOP				0x02 
+#define  DIR_ROOT_DOCUMENTS				0x04 
+#define  DIR_ROOT_DOWNLOADS				0x08 
+
+
+
+
+	/**
+	* \brief  Simple Enumeracion los Tipos de Directorio
+	*/
+	enum SystemFolder
+	{
+
+		WIN_FOLDERS = 0x00,
+		DIR_HOME_SYSTEM = WIN_FOLDERS | DIR_ROOT_SYSTEM, // C:\\WINDOWS
+		DIR_DESKTOP   = WIN_FOLDERS | DIR_ROOT_DESKTOP,
+		DIR_DOCUMENTS = WIN_FOLDERS | DIR_ROOT_DOCUMENTS,
+		DIR_DOWNLOADS = WIN_FOLDERS | DIR_ROOT_DOWNLOADS,
+
+
+		WIN_CUSTOM_FOLDERS = 0x33BD9B76,
+		DIR_FIREFOX = WIN_CUSTOM_FOLDERS | 0xFA0EACB1,
+		DIR_TOTAL = 0x08
+
+
+	};
+
+
 	class FileSystem
 	{
 	public:
 		FileSystem();
 		~FileSystem();
 
-	private:
+		bool getSytemDir(LPWSTR buffer, DWORD flags);
+		LPWSTR getWindowsPath();
+		LPWSTR getDesktopPath();
+		LPWSTR getDocumentsPath();
+		LPWSTR getDowloadsPath();
 
+
+
+	private:
+		WCHAR* path;
+		WCHAR dirPath[MAX_PATH];
 	};
 
 
